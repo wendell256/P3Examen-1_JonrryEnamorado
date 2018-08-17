cea9dd436ffe7fb00bb7d3cf5dd64413dcc30377
wendell256 <wendell256@gmail.com>
2018-08-17 14:05:11 -0600

    se crea, se inicializa y se imprime el tablero

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <string>
 using namespace std;
 char** createMatrix(int);
-
-
+void printMatrix(char**,int);
+void iniciarTablero(char**,int);
 
 int main(){
 	string player1, player2;
@@ -11,18 +11,48 @@ int main(){
 	cin>>player1;
 	cout<<"Ingrese nombre jugador 2:"<<endl;
 	cin>>player2;
-	createMatrix(11);
-
+	char** tablero=createMatrix(11);
+	iniciarTablero(tablero,11);
+	printMatrix(tablero,11);
 
 
 	return 0;
 }
 
+//crear matrix
 char** createMatrix(int size){
-	char** matrix;
-	for(int i;i<size;i++){
-		matrix[i]=new char**[size];
-	}
-	return matrix
+	char** matrix = new char*[size];
 
+        for(int i=0;i<size;i++)
+                matrix[i]= new char[size];
+
+        return matrix;
+
+
+}
+
+//crear tablero al inicio
+void iniciarTablero(char** tablero, int size){
+	for(int i=0;i<size;i++){
+		for(int j=0;j<size;j++){
+			if((i==0 && j==5)||(i==10&&j==5)){
+				tablero[i][j]='+';
+			}else if((i==5 && j==0)||(i==5&&j==10)){
+				tablero[i][j] = '#';
+			}else{
+				tablero[i][j]='.';
+			}
+
+		}
+	}
+}
+
+//imprimir matrix
+void printMatrix(char** matrix,int size){
+	for(int i=0;i<size;i++){
+		for (int j=0;j<size;j++){
+			cout<<matrix[i][j]<<" ";
+		}
+		cout<<endl;
+	}
 }
